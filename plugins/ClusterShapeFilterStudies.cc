3f85837e7bcc1eb63310e9b32943d5ea695c6527
Giovanni <gpetruc@gmail.com>
2016-06-04 11:36:21 +0200

    Layer-level information

--- plugins/ClusterShapeFilterStudies.cc
+++ plugins/ClusterShapeFilterStudies.cc
@@ -20,10 +20,14 @@
 #include "TrackingTools/PatternTools/interface/Trajectory.h"
 #include "DataFormats/SiStripCluster/interface/SiStripClusterTools.h"
 #include "DataFormats/TrackerRecHit2D/interface/TrackerSingleRecHit.h"
+#include "DataFormats/TrackerCommon/interface/TrackerTopology.h"
+#include "Geometry/Records/interface/TrackerTopologyRcd.h"
 
 namespace {
     static std::vector<std::string> sDETS{ "", "TIB", "TID", "TOB", "TEC" };
     static std::vector<unsigned>    iDETS{ 0,   3,      4,     5,     6   };
+    static std::vector<unsigned>    iLAYS{ 0,   4,      3,     6,     9   };
+    static std::vector<std::string> sLAYS{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 }
 
 class ClusterShapeFilterStudies : public edm::EDProducer {
@@ -35,8 +39,8 @@ private:
   virtual void produce(edm::Event&, const edm::EventSetup&);
 
   struct DetAndCharge { 
-        uint32_t det; float charge; 
-        DetAndCharge(uint32_t adet, float acharge) : det(adet), charge(acharge) {} 
+        uint32_t det, layer; float charge; 
+        DetAndCharge(uint32_t adet, uint32_t alayer, float acharge) : det(adet), layer(alayer), charge(acharge) {} 
         bool operator<(const DetAndCharge &other) const { return charge < other.charge; } 
   };
   std::vector<DetAndCharge> hitsOnTrack(const reco::Track &track, double estimateCut=9e9) const ;
@@ -53,6 +57,8 @@ private:
   const Hand & handle,
   const std::vector<T> & values,
   const std::string    & label) const ;
+
+  edm::ESHandle<TrackerTopology> theTrkTopo;
 };
 
 //
@@ -68,6 +74,12 @@ refitter_(iConfig)
       produces<edm::ValueMap<float> >("byCharge0"+sDETS[i]);
       produces<edm::ValueMap<float> >("byCharge1"+sDETS[i]);
       produces<edm::ValueMap<float> >("byCharge2"+sDETS[i]);
+      produces<edm::ValueMap<float> >("byCharge3"+sDETS[i]);
+      produces<edm::ValueMap<float> >("byCharge4"+sDETS[i]);
+      for (unsigned int j = 1; j <= iLAYS[i]; ++j) {
+          produces<edm::ValueMap<float> >("byLayerMin"+sDETS[i]+sLAYS[j]);
+          produces<edm::ValueMap<float> >("byLayerMax"+sDETS[i]+sLAYS[j]);
+      }    
   }
 }
 
@@ -85,15 +97,22 @@ ClusterShapeFilterStudies::produce(edm::Event& iEvent, const edm::EventSetup& iS
   Handle<View<reco::Muon> > probes;
   iEvent.getByToken(probes_, probes);
 
+  iSetup.get<TrackerTopologyRcd>().get(theTrkTopo);
+
   if (!probes->empty()) refitter_.setServices(iSetup);
 
   unsigned int n = probes->size();
-  std::vector<float> byCharge[5][3];
+  std::vector<float> byCharge[5][5];
+  std::vector<float> byLayerMin[5][10], byLayerMax[5][10];
   std::vector<float> median[5];
   for (unsigned int idet = 0; idet < 5; ++idet) {
       median[idet] = std::vector<float>(n,0);
-      for (unsigned int i = 0; i < 3; ++i) {
+      for (unsigned int i = 0; i < 5; ++i) {
           byCharge[idet][i] = std::vector<float>(n,0);
+      }
+      for (unsigned int i = 0; i < 10; ++i) {
+          byLayerMin[idet][i] = std::vector<float>(n,0);
+          byLayerMax[idet][i] = std::vector<float>(n,0);
       }
   }
 
@@ -105,14 +124,34 @@ ClusterShapeFilterStudies::produce(edm::Event& iEvent, const edm::EventSetup& iS
       for (int idet = 0; idet < 5; ++idet) {
           std::vector<DetAndCharge> hits;
           for (auto & h : hitsAll) { 
-            if (idet == 0 || h.det == iDETS[idet]) hits.push_back(h);
-         }
-          median[idet][i] = hits.empty() ? 9999. : (
+              if (idet == 0 || h.det == iDETS[idet]) hits.push_back(h);
+          }
+          median[idet][i] = hits.empty() ? 99999. : (
                   hits.size() % 2 == 1 ? hits[(hits.size()+1)/2].charge 
                   : 0.5*(hits[hits.size()/2].charge + hits[hits.size()/2+1].charge));
           byCharge[idet][0][i] = (hits.size() > 0 ? hits[0].charge : 999999.);
           byCharge[idet][1][i] = (hits.size() > 1 ? hits[1].charge : 999999.);
           byCharge[idet][2][i] = (hits.size() > 2 ? hits[2].charge : 999999.);
+          byCharge[idet][3][i] = (hits.size() > 3 ? hits[3].charge : 999999.);
+          byCharge[idet][4][i] = (hits.size() > 4 ? hits[4].charge : 999999.);
+          for (unsigned int j = 1; j <= iLAYS[idet]; ++j) {
+              bool found =false; 
+              float cmin = 99999, cmax = 99999;
+              for (auto & h : hitsAll) { 
+                  if (h.det == iDETS[idet] && h.layer == j) {
+                      if (!found) {
+                          cmin = h.charge;
+                          cmax = h.charge;
+                          found = true;
+                      } else {
+                          cmin = std::min(cmin, h.charge);
+                          cmax = std::max(cmax, h.charge);
+                      }
+                  }
+              }
+              byLayerMin[idet][j][i] = cmin;
+              byLayerMax[idet][j][i] = cmax;
+          }
       } 
   }
 
@@ -121,6 +160,12 @@ ClusterShapeFilterStudies::produce(edm::Event& iEvent, const edm::EventSetup& iS
       storeMap(iEvent, probes, byCharge[idet][0], "byCharge0"+sDETS[idet]);
       storeMap(iEvent, probes, byCharge[idet][1], "byCharge1"+sDETS[idet]);
       storeMap(iEvent, probes, byCharge[idet][2], "byCharge2"+sDETS[idet]);
+      storeMap(iEvent, probes, byCharge[idet][3], "byCharge3"+sDETS[idet]);
+      storeMap(iEvent, probes, byCharge[idet][4], "byCharge4"+sDETS[idet]);
+      for (unsigned int j = 1; j <= iLAYS[idet]; ++j) {
+          storeMap(iEvent, probes, byLayerMin[idet][j], "byLayerMin"+sDETS[idet]+sLAYS[j]);
+          storeMap(iEvent, probes, byLayerMax[idet][j], "byLayerMax"+sDETS[idet]+sLAYS[j]);
+      }    
   }
 }
 
@@ -139,7 +184,7 @@ ClusterShapeFilterStudies::hitsOnTrack(const reco::Track &track, double estimate
             float charge =  siStripClusterTools::chargePerCM(hit->geographicalId(), hit->stripCluster(), tsos.localParameters());
             //if (charge < 1945.0) std::cout << "Low charge hit on sub " << subdet << ", charge " << charge << ", chi2 " << tm.estimate() << std::endl;
             //std::cout << "subdet " << tm.recHitR().geographicalId().subdetId() << ", hit: " << typeid(tm.recHitR()).name() << ", charge " << charge << ", sub: " << subdet << std::endl;
-            ret.push_back(DetAndCharge(subdet,charge)); 
+            ret.push_back(DetAndCharge(subdet,theTrkTopo->layer(tm.recHitR().geographicalId()),charge)); 
         }
     }
     return ret;
